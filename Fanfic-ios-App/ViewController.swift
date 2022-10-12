863ce676ee28de0055ed697155ab3c645934b5ea
slavaglc <47712523+slavaglc@users.noreply.github.com>
2022-10-12 19:53:16 +0300

    Initial Commit

---
+++ Fanfic-ios-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fanfic-ios-App
+//
+//  Created by Вячеслав Макаров on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
